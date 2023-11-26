97b8f5cc535cdfd42429215a7ff89e614da42208
Saikumar guduru <skg.13.edu@gmail.com>
2023-11-26 15:38:31 +0530

    Your commit message

--- src/components/Admin/AdminInvoiceManager.js
+++ src/components/Admin/AdminInvoiceManager.js
@@ -32,7 +32,7 @@ function AdminInvoiceManagement() {
 
     const ViewInvoice = () => {
         if (selectedInvoiceId) {
-            window.location = `/invoice/${selectedInvoiceId}`;
+            window.location = `${API}download/${selectedInvoiceId}`;
         }
     };
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import "@fontsource/urbanist"; // Defaults to weight 400
+import "@fontsource/urbanist/400.css"; // Specify weight
+import "@fontsource/urbanist/400-italic.css"; // Specify weight and style
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>

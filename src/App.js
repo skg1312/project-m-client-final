e91e41a44d7c548d288d05754b3fd32eb6a342b4
Aakash Hadli <aakashhadli@gmail.com>
2023-11-27 08:40:54 +0530

    FIX: gap between data and form

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ function App() {
         {/* Admin Routes */}
         <AdminAuthProvider>
           <Routes>
-            <Route path='/' element={<AdminLogin />} />
+            {/* <Route path='/' element={<AdminLogin />} /> */}
             <Route path='/admindashboard' element={
               <ReqAdminAuth>
                 
@@ -216,7 +216,7 @@ function App() {
           } />
             <Route path='/staffinvview' element={
             <ReqStaffAuth>
-              {/* responsive */}
+              
               <StaffPdfViewer />
             </ReqStaffAuth>
           } />
@@ -303,7 +303,7 @@ function App() {
            } />
             <Route path='/userinvview' element={
             <ReqUserAuth>
-              {/* responsive */}
+              
               <UserPdfViewer />
             </ReqUserAuth>
            } />

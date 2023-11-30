11cc8fbd62d96c5dbeb54a61ee7d57e33ccfcf7f
Saikumar guduru <87139133+skg1312@users.noreply.github.com>
2023-11-30 12:04:15 +0530

    Update AdminBuyerManage.js

--- src/components/Admin/AdminBuyerManage.js
+++ src/components/Admin/AdminBuyerManage.js
@@ -10,8 +10,6 @@ function AdminBuyerManage() {
   const [pageNumber, setPageNumber] = useState(0);
   const [selectedBuyerId, setSelectedBuyerId] = useState(null);
 
-  const API = process.env.REACT_APP_API;
-
   const [selectedBuyerData, setSelectedBuyerData] = useState({
     buyerid: '',
     buyercompanyname: '',
@@ -21,16 +19,25 @@ function AdminBuyerManage() {
     buyercompanystatecode: '',
   });
 
+  const [file, setFile] = useState(null);
+
+  const API = process.env.REACT_APP_API;
+
   const itemsPerPage = 12;
   const [searchInput, setSearchInput] = useState('');
 
-  // Sort the buyer array in reverse order (newest first)
   const sortedBuyers = [...buyers].reverse();
   const displayedBuyerSearch = sortedBuyers
-  .filter(
-    (item) =>
-      item.buyercompanyname.toLowerCase().includes(searchInput.toLowerCase()))
-  .slice(pageNumber * itemsPerPage, (pageNumber + 1) * itemsPerPage);
+    .filter((item) => {
+      const buyerCompanyName = item.buyercompanyname || '';
+
+      if (buyerCompanyName.toLowerCase().includes(searchInput?.toLowerCase())) {
+        return true;
+      }
+
+      return false;
+    })
+    .slice(pageNumber * itemsPerPage, (pageNumber + 1) * itemsPerPage);
 
   const pageCount = Math.ceil(sortedBuyers.length / itemsPerPage);
 
@@ -40,7 +47,7 @@ function AdminBuyerManage() {
 
   useEffect(() => {
     axios
-      .get(`${API}buyer`) // Use the appropriate API endpoint for buyers
+      .get(`${API}buyer`)
       .then((response) => {
         setBuyers(response.data);
       })
@@ -59,13 +66,10 @@ function AdminBuyerManage() {
     e.preventDefault();
 
     if (selectedBuyerId) {
-      // Update an existing buyer
       axios
         .put(`${API}buyer/${selectedBuyerId}`, selectedBuyerData)
         .then((response) => {
-          // Handle successful update (if needed)
           console.log('Buyer updated successfully:', response.data);
-          // Optionally, you can update the local state to reflect the changes
           setBuyers((prevBuyers) =>
             prevBuyers.map((buyer) => (buyer._id === selectedBuyerId ? response.data : buyer))
           );
@@ -74,13 +78,10 @@ function AdminBuyerManage() {
           console.error('Error updating buyer:', error);
         });
     } else {
-      // Create a new buyer
       axios
         .post(`${API}buyer`, selectedBuyerData)
         .then((response) => {
-          // Handle successful creation (if needed)
           console.log('Buyer created successfully:', response.data);
-          // Optionally, you can update the local state to include the new buyer
           setBuyers((prevBuyers) => [...prevBuyers, response.data]);
         })
         .catch((error) => {
@@ -100,6 +101,27 @@ function AdminBuyerManage() {
     setSelectedBuyerId(null);
   };
 
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleFileUpload = async () => {
+    try {
+      const formData = new FormData();
+      formData.append('file', file);
+
+      const response = await axios.post(`${API}buyer/upload`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      console.log('File uploaded successfully:', response.data);
+    } catch (error) {
+      console.error('Error uploading file:', error);
+    }
+  };
+
   return (
     <div
       style={{
@@ -110,46 +132,60 @@ function AdminBuyerManage() {
       }}
     >
       <AdminNavbar />
-    <div className='admin-buyer-manage'>
-      <div className='admin-buyer-manage-data'>
-        <h1 className='admin-buyer-manage-data-title'>ALL BUYERS</h1>
-        <input
-              type='text'
-              placeholder='Search Buyer...'
-              className='admin-user-manage-form-input' // Search input placeholder
-              value={searchInput} // Bind the input value to the state
-              onChange={(e) => setSearchInput(e.target.value)} // Update the searchInput state as the user types
-            />
-        <table className='admin-buyer-manage-data-table'>
-          <thead className='admin-buyer-manage-data-table-head'>
-            <tr className='admin-buyer-manage-data-table-row-head'>
-              <th className='admin-buyer-manage-data-table-header'>Buyer Id</th>
-              <th className='admin-buyer-manage-data-table-header'>Company Name</th>
-              <th className='admin-buyer-manage-data-table-header'>GST NO</th>
-              <th className='admin-buyer-manage-data-table-header'>State Name</th>
-              <th className='admin-buyer-manage-data-table-header'>Action</th>
-            </tr>
-          </thead>
-          <tbody className='admin-buyer-manage-data-table-body'>
-            {displayedBuyerSearch.map((buyer) => (
-              <tr key={buyer._id} className='admin-buyer-manage-data-table-row-body'>
-                <td className='admin-buyer-manage-data-table-data highlight'>{buyer.buyerid.substring(0, 12)}</td>
-                <td className='admin-buyer-manage-data-table-data'>{buyer.buyercompanyname.substring(0, 12)}</td>
-                <td className='admin-buyer-manage-data-table-data'>{buyer.buyercompanygstno.substring(0, 12)}</td>
-                <td className='admin-buyer-manage-data-table-data'>{buyer.buyercompanystatename.substring(0, 12)}</td>
-                <td className='admin-buyer-manage-data-table-data'>
-                  <button
-                    className='admin-buyer-manage-data-table-button'
-                    onClick={() => handleBuyerUpdate(buyer._id)}
-                  >
-                    Update
-                  </button>
-                </td>
+      <div className='admin-buyer-manage'>
+        <div className='admin-buyer-manage-file-upload'>
+          <h1>File Upload</h1>
+          <input type='file' onChange={handleFileChange} />
+          <button onClick={handleFileUpload}>Upload File</button>
+        </div>
+
+        <div className='admin-buyer-manage-data'>
+          <h1 className='admin-buyer-manage-data-title'>ALL BUYERS</h1>
+          <input
+            type='text'
+            placeholder='Search Buyer...'
+            className='admin-user-manage-form-input'
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+          <table className='admin-buyer-manage-data-table'>
+            <thead className='admin-buyer-manage-data-table-head'>
+              <tr className='admin-buyer-manage-data-table-row-head'>
+                <th className='admin-buyer-manage-data-table-header'>Company Name</th>
+                <th className='admin-buyer-manage-data-table-header'>GST NO</th>
+                <th className='admin-buyer-manage-data-table-header'>State Name</th>
+                <th className='admin-buyer-manage-data-table-header'>State Code</th>
+                <th className='admin-buyer-manage-data-table-header'>Action</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-        <br />
+            </thead>
+            <tbody className='admin-buyer-manage-data-table-body'>
+              {displayedBuyerSearch.map((buyer) => (
+                <tr key={buyer._id} className='admin-buyer-manage-data-table-row-body'>
+                  <td className='admin-buyer-manage-data-table-data highlight'>
+                    {buyer.buyercompanyname?.substring(0, 12) ?? 'N/A'}
+                  </td>
+                  <td className='admin-buyer-manage-data-table-data'>
+                    {buyer.buyercompanygstno?.substring(0, 12) ?? 'N/A'}
+                  </td>
+                  <td className='admin-buyer-manage-data-table-data'>
+                    {buyer.buyercompanystatename?.substring(0, 12) ?? 'N/A'}
+                  </td>
+                  <td className='admin-buyer-manage-data-table-data'>
+                    {buyer.buyercompanystatecode?.substring(0, 12) ?? 'N/A'}
+                  </td>
+                  <td className='admin-buyer-manage-data-table-data'>
+                    <button
+                      className='admin-buyer-manage-data-table-button'
+                      onClick={() => handleBuyerUpdate(buyer._id)}
+                    >
+                      Update
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <br />
           <ReactPaginate
             className='pagination-container'
             previousLabel='Previous'
@@ -164,72 +200,71 @@ function AdminBuyerManage() {
             pageClassName='pagination-button'
             breakClassName='pagination-space'
           />
-        <br />
-      </div>
-      <div className='admin-buyer-manage-form'>
-        <h1 className='admin-buyer-manage-form-title'>{selectedBuyerId ? 'UPDATE BUYER' : 'ADD BUYER'}</h1>
-        <form className='admin-buyer-manage-form-form' onSubmit={handleFormSubmit}>
-          <input
-            type='text'
-            required
-            className='admin-buyer-manage-form-input'
-            placeholder='Buyer ID'
-            value={selectedBuyerData.buyerid || ''}
-            onChange={(e) => setSelectedBuyerData({ ...selectedBuyerData, buyerid: e.target.value })}
-          />
-          <input
-            type='text'
-            required
-            className='admin-buyer-manage-form-input'
-            placeholder='Company Name'
-            value={selectedBuyerData.buyercompanyname || ''}
-            onChange={(e) => setSelectedBuyerData({ ...selectedBuyerData, buyercompanyname: e.target.value })}
-          />
-          <input
-            type='text'
-            required
-            className='admin-buyer-manage-form-input'
-            placeholder='Company GST Number'
-            value={selectedBuyerData.buyercompanygstno || ''}
-            onChange={(e) => setSelectedBuyerData({ ...selectedBuyerData, buyercompanygstno: e.target.value })}
-          />
-          <input
-            type='text'
-            required
-            className='admin-buyer-manage-form-input'
-            placeholder='Company Address'
-            value={selectedBuyerData.buyercompanyaddress || ''}
-            onChange={(e) => setSelectedBuyerData({ ...selectedBuyerData, buyercompanyaddress: e.target.value })}
-          />
-          <input
-            type='text'
-            required
-            className='admin-buyer-manage-form-input'
-            placeholder='Company State Name'
-            value={selectedBuyerData.buyercompanystatename || ''}
-            onChange={(e) => setSelectedBuyerData({ ...selectedBuyerData, buyercompanystatename: e.target.value })}
-          />
-          <input
-            type='text'
-            required
-            className='admin-buyer-manage-form-input'
-            placeholder='Company State Code'
-            value={selectedBuyerData.buyercompanystatecode || ''}
-            onChange={(e) => setSelectedBuyerData({ ...selectedBuyerData, buyercompanystatecode: e.target.value })}
-          />
           <br />
-          <input type='checkbox' required className='buyer-manage-form-input-checkbox' />
-          <label className='admin-buyer-manage-form-input-checkbox-label'>
-            I you agree with Terms and Conditions & Privacy Policy
-          </label>
-          <br />
-          <button type='submit' className='admin-buyer-manage-form-button'>
-            {selectedBuyerId ? 'Update' : 'Add'}
-          </button>
-        </form>
+        </div>
+        <div className='admin-buyer-manage-form'>
+          <h1 className='admin-buyer-manage-form-title'>{selectedBuyerId ? 'UPDATE BUYER' : 'ADD BUYER'}</h1>
+          <form className='admin-buyer-manage-form-form' onSubmit={handleFormSubmit}>
+          <input
+              type='text'
+              required
+              className='admin-buyer-manage-form-input'
+              placeholder='Buyer ID'
+              value={selectedBuyerData.buyerid || ''}
+              onChange={(e) => setSelectedBuyerData({ ...selectedBuyerData, buyerid: e.target.value })}
+            />
+            <input
+              type='text'
+              required
+              className='admin-buyer-manage-form-input'
+              placeholder='Company Name'
+              value={selectedBuyerData.buyercompanyname || ''}
+              onChange={(e) => setSelectedBuyerData({ ...selectedBuyerData, buyercompanyname: e.target.value })}
+            />
+            <input
+              type='text'
+              required
+              className='admin-buyer-manage-form-input'
+              placeholder='Company GST Number'
+              value={selectedBuyerData.buyercompanygstno || ''}
+              onChange={(e) => setSelectedBuyerData({ ...selectedBuyerData, buyercompanygstno: e.target.value })}
+            />
+            <input
+              type='text'
+              required
+              className='admin-buyer-manage-form-input'
+              placeholder='Company Address'
+              value={selectedBuyerData.buyercompanyaddress || ''}
+              onChange={(e) => setSelectedBuyerData({ ...selectedBuyerData, buyercompanyaddress: e.target.value })}
+            />
+            <input
+              type='text'
+              required
+              className='admin-buyer-manage-form-input'
+              placeholder='Company State Name'
+              value={selectedBuyerData.buyercompanystatename || ''}
+              onChange={(e) => setSelectedBuyerData({ ...selectedBuyerData, buyercompanystatename: e.target.value })}
+            />
+            <input
+              type='text'
+              required
+              className='admin-buyer-manage-form-input'
+              placeholder='Company State Code'
+              value={selectedBuyerData.buyercompanystatecode || ''}
+              onChange={(e) => setSelectedBuyerData({ ...selectedBuyerData, buyercompanystatecode: e.target.value })}
+            />
+            <br />
+            <input type='checkbox' required className='buyer-manage-form-input-checkbox' />
+            <label className='admin-buyer-manage-form-input-checkbox-label'>
+              I you agree with Terms and Conditions & Privacy Policy
+            </label>
+            <br />
+            <button type='submit' className='admin-buyer-manage-form-button'>
+              {selectedBuyerId ? 'Update' : 'Add'}
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
-
     </div>
   );
 }

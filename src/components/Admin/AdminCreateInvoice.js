beaa8f32a58d804046ce977f8c0acfcf382d6a65
Saikumar guduru <87139133+skg1312@users.noreply.github.com>
2023-12-17 18:23:13 +0530

    Update AdminCreateInvoice.js

--- src/components/Admin/AdminCreateInvoice.js
+++ src/components/Admin/AdminCreateInvoice.js
@@ -712,6 +712,7 @@ function AdminCreateInvoice() {
 								id='vechiclenuumber'
 								name='vechiclenuumber'
 								type='text'
+								pattern="^[A-Z]{2}[0-9]{2}[A-Z]{2}[0-9]{4}$"
 								required
 								onChange={(e) =>
 									handleChange(e, 'vehicledetails', 'vechiclenuumber')

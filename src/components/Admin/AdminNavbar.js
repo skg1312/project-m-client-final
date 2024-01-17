8931bdb5f9b9588b83c15b7e47487eb24acea539
Aakash Hadli <aakashhadli@gmail.com>
2024-01-17 16:24:08 +0530

    mis print

--- src/components/Admin/AdminNavbar.js
+++ src/components/Admin/AdminNavbar.js
@@ -1,57 +1,90 @@
-import React from "react";
+import React from 'react';
 import './AdminNavbar.css';
 import { useNavigate } from 'react-router-dom';
 import { useAdminAuth } from './AdminAuth';
 
 function AdminNavbar() {
-    const navigate = useNavigate();
-    const auth = useAdminAuth();
+	const navigate = useNavigate();
+	const auth = useAdminAuth();
 
-  const handleLogout = () => {
-    auth.adminlogout();
+	const handleLogout = () => {
+		auth.adminlogout();
+	};
+
+	return (
+		<div className='admin-logout-box-nav'>
+			<div className='admin-logout-container-nav'>
+				<div className='admin-logout-button-nav'>
+					<button
+						className='admin-logout-button-value-no-img'
+						onClick={() => navigate('/admingenrep')}
+					>
+						REPORTS
+					</button>
+					<button
+						className='admin-logout-button-value-no-img'
+						onClick={() => navigate('/adminuserman')}
+					>
+						USERS
+					</button>
+					<button
+						className='admin-logout-button-value-no-img'
+						onClick={() => navigate('/adminstaffman')}
+					>
+						STAFF
+					</button>
+					<button
+						className='admin-logout-button-value-no-img'
+						onClick={() => navigate('/admincomman')}
+					>
+						COMPANY
+					</button>
+					<button
+						className='admin-logout-button-value-no-img'
+						onClick={() => navigate('/adminsellman')}
+					>
+						AGENT
+					</button>
+					<button
+						className='admin-logout-button-value-no-img'
+						onClick={() => navigate('/adminbuyman')}
+					>
+						BUYER
+					</button>
+					<button
+						className='admin-logout-button-value-no-img'
+						onClick={() => navigate('/adminconman')}
+					>
+						ITEMS
+					</button>
+					<button
+						className='admin-logout-button-value-no-img'
+						onClick={() => navigate('/adminvecman')}
+					>
+						OTHERS
+					</button>
+					<button
+						className='admin-logout-button-value-no-img'
+						onClick={() => navigate('/admininvdash')}
+					>
+						INOVICE
+					</button>
+					<button
+						className='admin-logout-button-value-no-img'
+						onClick={() => navigate('/admindashboard')}
+					>
+						DASHBOARD
+					</button>
+					<button
+						className='admin-logout-button-value-no-img'
+						onClick={handleLogout}
+					>
+						LOGOUT
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
-
-    return (
-        <div className='admin-logout-box-nav'>
-          <div className='admin-logout-container-nav'>
-            <div className='admin-logout-button-nav'>
-            <button className='admin-logout-button-value-no-img'  onClick={() => navigate('/admingenrep')}>
-                REPORTS
-              </button>
-              <button className='admin-logout-button-value-no-img'  onClick={() => navigate('/adminuserman')}>
-                USERS
-              </button>
-              <button className='admin-logout-button-value-no-img'  onClick={() => navigate('/adminstaffman')}>
-                STAFF
-              </button>
-              <button className='admin-logout-button-value-no-img'  onClick={() => navigate('/admincomman')}>
-                COMPANY
-              </button>
-              <button className='admin-logout-button-value-no-img'  onClick={() => navigate('/adminsellman')}>
-                AGENT
-              </button>
-              <button className='admin-logout-button-value-no-img'  onClick={() => navigate('/adminbuyman')}>
-                BUYER
-              </button>
-              <button className='admin-logout-button-value-no-img'  onClick={() => navigate('/adminconman')}>
-                CONSIGNMENT
-              </button>
-              <button className='admin-logout-button-value-no-img'  onClick={() => navigate('/adminvecman')}>
-                LOADING
-              </button>
-              <button className='admin-logout-button-value-no-img'  onClick={() => navigate('/admininvdash')}>
-                INOVICE
-              </button>
-              <button className='admin-logout-button-value-no-img'  onClick={() => navigate('/admindashboard')}>
-                DASHBOARD
-              </button>
-                <button className='admin-logout-button-value-no-img'  onClick={handleLogout}>
-                LOGOUT
-              </button>
-            </div>
-          </div>
-        </div>
-    )
-    }
 
 export default AdminNavbar;
